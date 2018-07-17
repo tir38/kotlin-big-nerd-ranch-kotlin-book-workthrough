cb85a3e7b3bf2f18075f3ed72bb45c69c619ff13
Jason Atwood <jwatwood@gmail.com>
2018-07-16 22:03:14 -0400

    Finished ch2

---
+++ sandbox/src/TypeIntro.kt
@@ -0,0 +1,22 @@
+const val MAX_EXPERIENCE: Int = 500
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+    var hasSteed = false
+
+    var pubName = "Unicorn's Horn"
+    var publican = "current publican"
+    var currentGold = 50
+
+    var menu: List<String> = listOf("mead", "wine", "la croix")
+
+
+
+    println(experiencePoints)
+    println(playerName)
+    println(hasSteed)
+
+    println(playerName.reversed())
+}
