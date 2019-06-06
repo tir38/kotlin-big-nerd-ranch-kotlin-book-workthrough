670622281931a53675122f7c546abfd4e5f67859
Jason Atwood <jwatwood@gmail.com>
2019-06-05 22:00:43 -0400

    Finish ch7

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,24 +1,63 @@
+const val TAVERN_NAME: String = "Taernyl's Folly"
+
+
 fun main(args: Array<String>) {
-    var beverage = readLine()
-
-    beverage = null
-
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that, beverage is null")
-    }
-
-    // let example
-//    beverage = beverage?.let {
-//                if (it.isNotBlank()) {
-//        it.capitalize()
-//                } else {
-//                    "Buttered Ale"
-//                }
+//    var beverage = readLine()
+//    beverage = null
+//
+//    if (beverage != null) {
+//        beverage = beverage.capitalize()
+//    } else {
+//        println("I can't do that, beverage is null")
 //    }
+//
+//    // let example
+////    beverage = beverage?.let {
+////                if (it.isNotBlank()) {
+////        it.capitalize()
+////                } else {
+////                    "Buttered Ale"
+////                }
+////    }
+//
+//    val beverageServed: String = beverage ?: "Buttered Ale"
+//    println(beverageServed)
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
 
-    val beverageServed: String = beverage ?: "Buttered Ale"
+private fun toDragonSpeak(phrase: String) =
+        phrase.replace(Regex("[Aaeiou]")) {
+            when (it.value) {
+                "a" -> "4"
+                "A" -> "4"
+                "e" -> "3"
+                "i" -> "1"
+                "o" -> "0"
+                "u" -> "|_|"
+                else -> it.value
+            }
+        }
 
-    println(beverageServed)
+fun placeOrder(metaMenuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+//    val data = metaMenuData.split(",")
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+    val (type, name, price) = metaMenuData.split(",")
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+//    val phrase = "Ah, delicious $name"
+//    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah delicious $name")}"
+    } else {
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
 }
